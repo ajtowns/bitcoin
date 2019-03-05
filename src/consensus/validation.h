bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

[refactor] Drop redundant nDoS, corruptionPossible, SetCorruptionPossible

src/consensus/validation.h
@@ -75,25 +75,21 @@ private:
         MODE_ERROR,   //!< run-time error
     } mode;
     ValidationInvalidReason reason;
-    int nDoS;
     std::string strRejectReason;
     unsigned int chRejectCode;
-    bool corruptionPossible;
     std::string strDebugMessage;
 public:
-    CValidationState() : mode(MODE_VALID), reason(ValidationInvalidReason::NONE), nDoS(0), chRejectCode(0), corruptionPossible(false) {}
+    CValidationState() : mode(MODE_VALID), reason(ValidationInvalidReason::NONE), chRejectCode(0) {}
     bool DoS(int level, ValidationInvalidReason reasonIn, bool ret = false,
              unsigned int chRejectCodeIn=0, const std::string &strRejectReasonIn="",
-             bool corruptionIn=false,
+             bool corruptionPossibleIn=false,
              const std::string &strDebugMessageIn="") {
         reason = reasonIn;
         chRejectCode = chRejectCodeIn;
         strRejectReason = strRejectReasonIn;
-        corruptionPossible = corruptionIn;
         strDebugMessage = strDebugMessageIn;
-        nDoS = level;
-        assert(nDoS == GetDoSForReason());
-        assert(corruptionPossible == (reason == ValidationInvalidReason::BLOCK_MUTATED || reason == ValidationInvalidReason::TX_WITNESS_MUTATED));
+        assert(level == GetDoS());
+        assert(corruptionPossibleIn == CorruptionPossible());
         if (mode == MODE_ERROR)
             return ret;
         mode = MODE_INVALID;
@@ -120,15 +116,9 @@ public:
         return mode == MODE_ERROR;
     }
     bool CorruptionPossible() const {
-        assert(corruptionPossible == (reason == ValidationInvalidReason::BLOCK_MUTATED || reason == ValidationInvalidReason::TX_WITNESS_MUTATED));
-        return corruptionPossible;
+        return reason == ValidationInvalidReason::BLOCK_MUTATED || reason == ValidationInvalidReason::TX_WITNESS_MUTATED;
     }
-    void SetCorruptionPossible() {
-        corruptionPossible = true;
-        assert(corruptionPossible == (reason == ValidationInvalidReason::BLOCK_MUTATED || reason == ValidationInvalidReason::TX_WITNESS_MUTATED));
-    }
-    int GetDoS(void) const { return nDoS; }
-    int GetDoSForReason() const {
+    int GetDoS() const {
         switch (reason) {
         case ValidationInvalidReason::NONE:
             return 0;

ajtowns/bitcoin

src/consensus/validation.h
@@ -75,25 +75,21 @@ private:
         MODE_ERROR,   //!< run-time error
     } mode;
     ValidationInvalidReason m_reason;
-    int nDoS;
     std::string strRejectReason;
     unsigned int chRejectCode;
-    bool corruptionPossible;
     std::string strDebugMessage;
 public:
-    CValidationState() : mode(MODE_VALID), m_reason(ValidationInvalidReason::NONE), nDoS(0), chRejectCode(0), corruptionPossible(false) {}
+    CValidationState() : mode(MODE_VALID), m_reason(ValidationInvalidReason::NONE), chRejectCode(0) {}
     bool DoS(int level, ValidationInvalidReason reason, bool ret = false,
              unsigned int chRejectCodeIn=0, const std::string &strRejectReasonIn="",
-             bool corruptionIn=false,
+             bool corruptionPossibleIn=false,
              const std::string &strDebugMessageIn="") {
         m_reason = reason;
         chRejectCode = chRejectCodeIn;
         strRejectReason = strRejectReasonIn;
-        corruptionPossible = corruptionIn;
         strDebugMessage = strDebugMessageIn;
-        nDoS = level;
-        assert(nDoS == GetDoSForReason());
-        assert(corruptionPossible == (reason == ValidationInvalidReason::BLOCK_MUTATED || m_reason == ValidationInvalidReason::TX_WITNESS_MUTATED));
+        assert(level == GetDoS());
+        assert(corruptionPossibleIn == CorruptionPossible());
         if (mode == MODE_ERROR)
             return ret;
         mode = MODE_INVALID;
@@ -120,15 +116,9 @@ public:
         return mode == MODE_ERROR;
     }
     bool CorruptionPossible() const {
-        assert(corruptionPossible == (m_reason == ValidationInvalidReason::BLOCK_MUTATED || m_reason == ValidationInvalidReason::TX_WITNESS_MUTATED));
-        return corruptionPossible;
+        return m_reason == ValidationInvalidReason::BLOCK_MUTATED || m_reason == ValidationInvalidReason::TX_WITNESS_MUTATED;
     }
-    void SetCorruptionPossible() {
-        corruptionPossible = true;
-        assert(m_reason == ValidationInvalidReason::BLOCK_MUTATED || m_reason == ValidationInvalidReason::TX_WITNESS_MUTATED);
-    }
-    int GetDoS(void) const { return nDoS; }
-    int GetDoSForReason() const {
+    int GetDoS() const {
         switch (m_reason) {
         case ValidationInvalidReason::NONE:
             return 0;