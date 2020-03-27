ccfd591591541297868ba4ed1b391f5774d28835
Fabian Jahr <fjahr@protonmail.com>
2020-03-27 02:12:47 +0100

    test: Update test framework p2p protocol version

--- test/functional/test_framework/mininode.py
+++ test/functional/test_framework/mininode.py
@@ -51,6 +51,7 @@ from test_framework.messages import (
     MSG_TYPE_MASK,
     msg_verack,
     msg_version,
+    msg_wtxidrelay,
     NODE_NETWORK,
     NODE_WITNESS,
     sha256,
@@ -84,6 +85,7 @@ MESSAGEMAP = {
     b"tx": msg_tx,
     b"verack": msg_verack,
     b"version": msg_version,
+    b"wtxidrelay": msg_wtxidrelay,
 }
 
 MAGIC_BYTES = {
@@ -340,6 +342,7 @@ class P2PInterface(P2PConnection):
     def on_sendcmpct(self, message): pass
     def on_sendheaders(self, message): pass
     def on_tx(self, message): pass
+    def on_wtxidrelay(self, message): pass
 
     def on_inv(self, message):
         want = msg_getdata()
