512e0f9076b5d22f44d2a6dbf4eaacc1bdeb789b
Pieter Wuille <pieter@wuille.net>
2020-09-20 21:18:09 -0700

    Add txrequest unit tests
    Add unit tests for TxRequestTracker. Several scenarios are tested,
    randomly interleaved with eachother.

--- src/txrequest.h
+++ src/txrequest.h
@@ -194,6 +194,18 @@ public:
 
     /** Count how many announcements are being tracked in total across all peers and transactions. */
     size_t Size() const;
+
+    /** Access to the internal priority computation (testing only) */
+    uint64_t ComputePriority(const uint256& txhash, uint64_t peer, bool preferred, bool first) const;
+
+    /** Run internal consistency check (testing only). */
+    void SanityCheck() const;
+
+    /** Run a time-dependent internal consistency check (testing only).
+     *
+     * This can only be called immediately after GetRequestable, with the same 'now' parameter.
+     */
+    void PostGetRequestableSanityCheck(std::chrono::microseconds now) const;
 };
 
 #endif // BITCOIN_TXREQUEST_H
