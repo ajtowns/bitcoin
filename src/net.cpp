ce058d32ffe90e803558e658ac807c1d8cdc1751
Anthony Towns <aj@erisian.com.au>
2021-04-20 15:49:48 +1000

    net: drop cs_sendProcessing
    SendMessages() is now protected internally by m_mutex_message_handling;
    so this additional lock is not useful.

--- src/net.cpp
+++ src/net.cpp
@@ -2195,10 +2195,7 @@ void CConnman::ThreadMessageHandler()
             if (flagInterruptMsgProc)
                 return;
             // Send messages
-            {
-                LOCK(pnode->cs_sendProcessing);
-                m_msgproc->SendMessages(pnode);
-            }
+            m_msgproc->SendMessages(pnode);
 
             if (flagInterruptMsgProc)
                 return;
