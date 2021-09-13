48ae4c121736a0bbe9480b1b0aa41b087e5d3077
Carl Dong <contact@carldong.me>
2021-09-13 17:02:55 -0400

    init: Reset mempool and chainman via reconstruction
    Fixes https://github.com/bitcoin/bitcoin/issues/22964
    
    Previously, we used UnloadBlockIndex() in order to reset node.mempool
    and node.chainman. However, that has proven to be fragile (see
    https://github.com/bitcoin/bitcoin/issues/22964), and requires
    UnloadBlockIndex and its callees to be updated manually for each member
    that's introduced to the mempool and chainman classes.
    
    In this commit, we stop using the UnloadBlockIndex function and we
    simply reconstruct node.mempool and node.chainman.
    
    Since PeerManager needs a valid reference to both node.mempool and
    node.chainman, we also move PeerManager's construction via `::make` to
    after the chainstate activation sequence is complete.
    
    There are no more callers to UnloadBlockIndex after this commit, so it
    and its sole callees can be pruned.

--- src/init.cpp
+++ src/init.cpp
@@ -1265,18 +1265,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
     // as they would never get updated.
     if (!ignores_incoming_txs) node.fee_estimator = std::make_unique<CBlockPolicyEstimator>();
 
-    assert(!node.mempool);
     int check_ratio = std::min<int>(std::max<int>(args.GetIntArg("-checkmempool", chainparams.DefaultConsistencyChecks() ? 1 : 0), 0), 1000000);
-    node.mempool = std::make_unique<CTxMemPool>(node.fee_estimator.get(), check_ratio);
-
-    assert(!node.chainman);
-    node.chainman = std::make_unique<ChainstateManager>();
-    ChainstateManager& chainman = *node.chainman;
-
-    assert(!node.peerman);
-    node.peerman = PeerManager::make(chainparams, *node.connman, *node.addrman, node.banman.get(),
-                                     chainman, *node.mempool, ignores_incoming_txs);
-    RegisterValidationInterface(node.peerman.get());
 
     // sanitize comments per BIP-0014, format user agent and check total size
     std::vector<std::string> uacomments;
@@ -1397,8 +1386,18 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
     LogPrintf("* Using %.1f MiB for chain state database\n", cache_sizes.coins_db * (1.0 / 1024 / 1024));
     LogPrintf("* Using %.1f MiB for in-memory UTXO set (plus up to %.1f MiB of unused mempool space)\n", cache_sizes.coins * (1.0 / 1024 / 1024), nMempoolSizeMax * (1.0 / 1024 / 1024));
 
+    assert(!node.mempool);
+    assert(!node.chainman);
     bool fLoaded = false;
     while (!fLoaded && !ShutdownRequested()) {
+        node.mempool = std::make_unique<CTxMemPool>(node.fee_estimator.get(), check_ratio);
+        CTxMemPool& mempool = *Assert(node.mempool);
+
+        node.chainman = std::make_unique<ChainstateManager>();
+        ChainstateManager& chainman = *Assert(node.chainman);
+        chainman.m_total_coinstip_cache = cache_sizes.coins;
+        chainman.m_total_coinsdb_cache = cache_sizes.coins_db;
+
         const bool fReset = fReindex;
         bilingual_str strLoadError;
 
@@ -1408,7 +1407,7 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
         try {
             maybe_load_error = LoadChainstate(fReset,
                                               chainman,
-                                              Assert(node.mempool.get()),
+                                              &mempool,
                                               fPruneMode,
                                               chainparams.GetConsensus(),
                                               fReindexChainState,
@@ -1528,6 +1527,13 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
         LogPrintf("Shutdown requested. Exiting.\n");
         return false;
     }
+
+    ChainstateManager& chainman = *Assert(node.chainman);
+
+    assert(!node.peerman);
+    node.peerman = PeerManager::make(chainparams, *node.connman, *node.addrman, node.banman.get(),
+                                     chainman, *node.mempool, ignores_incoming_txs);
+    RegisterValidationInterface(node.peerman.get());
 
     // ********************************************************* Step 8: start indexers
     if (args.GetBoolArg("-txindex", DEFAULT_TXINDEX)) {
