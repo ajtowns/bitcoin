9f9381b3b12d6b196d875144831d7be695e9da1d
Armin Sabouri <armins88@gmail.com>
2025-07-09 21:54:58 +1000

    Re-enable OP_CAT

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -520,6 +520,21 @@ bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& 
                 case OP_NOP:
                     break;
 
+                case OP_CAT:
+                {
+                    assert(flags & SCRIPT_VERIFY_OP_CAT); // unreachable otherwise
+
+                    if (stack.size() < 2)
+                        return set_error(serror, SCRIPT_ERR_INVALID_STACK_OPERATION);
+                    valtype& vch1 = stacktop(-2);
+                    valtype& vch2 = stacktop(-1);
+                    if (vch1.size() + vch2.size() > MAX_SCRIPT_ELEMENT_SIZE)
+                        return set_error(serror, SCRIPT_ERR_PUSH_SIZE);
+                    vch1.insert(vch1.end(), vch2.begin(), vch2.end());
+                    stack.pop_back();
+                    break;
+                }
+
                 case OP_CHECKLOCKTIMEVERIFY:
                 {
                     if (!(flags & SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY)) {
