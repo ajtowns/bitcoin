645a9fb2226a87d595e3f4f8d3a3d665dd28cb48
Anthony Towns <aj@erisian.com.au>
2025-12-03 01:17:47 +1000

    rpc/net: Use peerman.SendMessagetoPeer instead of connman.PushMessage for sendmsgtopeer

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -69,6 +69,7 @@
 #include <cstddef>
 #include <deque>
 #include <exception>
+#include <forward_list>
 #include <functional>
 #include <future>
 #include <initializer_list>
@@ -415,6 +416,10 @@ struct Peer {
      * timestamp the peer sent in the version message. */
     std::atomic<std::chrono::seconds> m_time_offset{0s};
 
+    /** Keep track of external attempts to push messages */
+    Mutex m_sendmsg_mutex;
+    std::forward_list<std::pair<CSerializedNetMsg, std::promise<bool>>> m_sendmsg;
+
     explicit Peer(NodeId id, ServiceFlags our_services, bool is_inbound)
         : m_id{id}
         , m_our_services{our_services}
@@ -551,6 +556,8 @@ public:
     void UnitTestMisbehaving(NodeId peer_id) override EXCLUSIVE_LOCKS_REQUIRED(!m_peer_mutex) { Misbehaving(*Assert(GetPeerRef(peer_id)), ""); };
     void UpdateLastBlockAnnounceTime(NodeId node, int64_t time_in_seconds) override;
     ServiceFlags GetDesirableServiceFlags(ServiceFlags services) const override;
+
+    std::future<bool> SendMessageToPeer(NodeId node, CSerializedNetMsg&& msg) override EXCLUSIVE_LOCKS_REQUIRED(!m_peer_mutex);
 
 private:
     void ProcessMessage(Peer& peer, CNode& pfrom, const std::string& msg_type, DataStream& vRecv, std::chrono::microseconds time_received,
@@ -5168,6 +5175,20 @@ bool PeerManagerImpl::MaybeDiscourageAndDisconnect(CNode& pnode, Peer& peer)
     return true;
 }
 
+std::future<bool> PeerManagerImpl::SendMessageToPeer(NodeId nodeid, CSerializedNetMsg&& msg)
+{
+    std::promise<bool> result;
+    auto fut = result.get_future();
+    PeerRef peer = GetPeerRef(nodeid);
+    if (peer == nullptr) {
+        result.set_value(false);
+    } else {
+        LOCK(peer->m_sendmsg_mutex);
+        peer->m_sendmsg.emplace_front(std::move(msg), std::move(result));
+    }
+    return fut;
+}
+
 bool PeerManagerImpl::ProcessMessages(CNode& node, std::atomic<bool>& interruptMsgProc)
 {
     AssertLockNotHeld(m_tx_download_mutex);
@@ -5800,6 +5821,21 @@ bool PeerManagerImpl::SendMessages(CNode& node)
         LogDebug(BCLog::NET, "addrfetch connection timeout, %s", node.DisconnectMsg());
         node.fDisconnect = true;
         return true;
+    }
+
+    {
+        decltype(peer.m_sendmsg) steal;
+        {
+            LOCK(peer.m_sendmsg_mutex);
+            steal.swap(peer.m_sendmsg);
+        }
+        steal.reverse();
+        while (!steal.empty()) {
+            auto [msg, result] = std::move(steal.front());
+            steal.pop_front();
+            PushMessage(node, std::move(msg));
+            result.set_value(true);
+        }
     }
 
     MaybeSendPing(node, peer, current_time);
