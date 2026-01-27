9e0175ba8104e12dae971b5a48bb8c9cd5778381
Anthony Towns <aj@erisian.com.au>
2026-01-27 22:29:30 +1000

    wip for STALEBLOCK message

--- src/protocol.h
+++ src/protocol.h
@@ -268,6 +268,12 @@ inline constexpr const char* SENDTXRCNCL{"sendtxrcncl"};
  * BIP 434 Peer feature negotiation
  */
 inline constexpr const char* FEATURE{"feature"};
+/**
+ * Announce a stale block header. Contents is an 80 byte header,
+ * followed by either 0 (if only the header is known) or the
+ * witness serialized size of the block.
+ */
+inline constexpr const char* STALEBLOCK{"staleblock"};
 }; // namespace NetMsgType
 
 /** All known message types (see above). Keep this in the same order as the list of messages above. */
@@ -308,12 +314,14 @@ inline const std::array ALL_NET_MESSAGE_TYPES{std::to_array<std::string>({
     NetMsgType::WTXIDRELAY,
     NetMsgType::SENDTXRCNCL,
     NetMsgType::FEATURE,
+    NetMsgType::STALEBLOCK,
 })};
 
 static constexpr size_t MAX_FEATUREID_LENGTH{80};
 static constexpr size_t MAX_FEATUREDATA_LENGTH{512};
 
 namespace NetMsgFeature {
+inline constexpr std::string_view STALEBLOCKS{"https://github.com/ajtowns/bitcoin/tree/202601-staleblocks"};
 }
 
 /** nServices flags */
