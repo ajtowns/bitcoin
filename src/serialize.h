de37b9e12e07484c2cb64e2638e0c94dc9bd12ac
Anthony Towns <aj@erisian.com.au>
2026-01-30 17:15:08 +1000

    serialize: Add FromStream helper
    Allows writing `Foo foo; x << vRecv;` as `auto foo =
    FromStream<Foo>(vRecv)`. This also allows declaring `foo` as const.

--- src/serialize.h
+++ src/serialize.h
@@ -1200,6 +1200,14 @@ public:
     }
 };
 
+template<typename T, typename Stream>
+T FromStream(Stream&& s)
+{
+    T result;
+    s >> result;
+    return result;
+}
+
 /**
  * Helper macro for SerParams structs
  *
