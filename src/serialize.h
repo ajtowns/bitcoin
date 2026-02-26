9f067f55d8bc8b7de5b607306fcb69ee02293cd7
Anthony Towns <aj@erisian.com.au>
2026-02-26 20:53:36 +1000

    serialize: string_view serialization

--- src/serialize.h
+++ src/serialize.h
@@ -691,6 +691,12 @@ template<typename Stream, typename C> void Serialize(Stream& os, const std::basi
 template<typename Stream, typename C> void Unserialize(Stream& is, std::basic_string<C>& str);
 
 /**
+ *  string_view
+ */
+template<typename Stream, typename C> void Serialize(Stream& os, const std::basic_string_view<C>& str);
+template<typename Stream, typename C> void Unserialize(Stream& is, std::basic_string_view<C>& str) = delete;
+
+/**
  * prevector
  */
 template<typename Stream, unsigned int N, typename T> inline void Serialize(Stream& os, const prevector<N, T>& v);
@@ -792,7 +798,16 @@ void Unserialize(Stream& is, std::basic_string<C>& str)
         is.read(MakeWritableByteSpan(str));
 }
 
-
+/**
+ * string_view
+ */
+template<typename Stream, typename C>
+void Serialize(Stream& os, const std::basic_string_view<C>& str)
+{
+    WriteCompactSize(os, str.size());
+    if (!str.empty())
+        os.write(MakeByteSpan(str));
+}
 
 /**
  * prevector
