117c0d27918b71833c7f5a14db864aec9cdd4169
Anthony Towns <aj@erisian.com.au>
2026-04-02 02:13:30 +1000

    fix: getpeerinfo func test

--- test/functional/rpc_net.py
+++ test/functional/rpc_net.py
@@ -180,6 +180,7 @@ class NetTest(BitcoinTestFramework):
                 "timeoffset": 0,
                 "transport_protocol_type": "v1" if not self.options.v2transport else "v2",
                 "version": 0,
+                "template_status": "unsupported",
             },
         )
         no_version_peer.peer_disconnect()
